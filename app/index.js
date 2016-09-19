d60b308c964ff1ec86c737f27d3b783508458a7d
xuebao555 <xuebao555@gmail.com>
2016-09-20 00:25:50 +0800

    web app script

--- app/index.js
+++ app/index.js
@@ -4,6 +4,12 @@ import ReactDOM from 'react-dom'
 import {Router, browserHistory} from 'react-router'
 import {syncHistoryWithStore} from 'react-router-redux'
 import {Provider} from 'react-redux'
+
+// import 'jquery/dist/jquery.min'
+// import 'jquery-ui-dist/jquery-ui.min'
+// import 'bootstrap/dist/js/bootstrap.min'
+ import 'bootstrap/dist/css/bootstrap.min.css'
+
 import route from './route'
 import configureStore from './store/configureStore'
 
