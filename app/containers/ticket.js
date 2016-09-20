d323ba6ee2c1af6dc2b9855b29ad55499fd51c6a
xuebao555 <xuebao555@gmail.com>
2016-09-20 15:30:37 +0800

    update containers

--- app/containers/ticket.js
+++ app/containers/ticket.js
@@ -1,6 +1,39 @@
-import { connect } from 'react-redux';
+import React, {Component, PropTypes} from 'react'
+import {connect} from 'react-redux'
+import { Link } from 'react-router'
+import AvailableTicket from '../components/availableticket'
+import BuyTicket from '../components/buyticket'
+import Header from '../components/header'
+import UsedTicket from '../components/usedticket'
 
-import Ticket from '../components/ticket'
+
+class Ticket extends Component {
+
+    render() {
+        return (
+            <div>
+                <Header/>
+                <div className="content height100vh signupContent">
+                    <div className="background">
+                        <div className="col-md-4 col-md-offset-4 mainContent">
+                            <ul className="nav nav-tabs blackBackground noborder">
+                                <li className="col-md-4 col-xs-4 text-center active"><a data-toggle="tab" href="#home">Available Tickets</a></li>
+                                <li className="col-md-4 col-xs-4 text-center"><a data-toggle="tab" href="#butyticket">Buy tickets</a></li>
+                                <li className="col-md-4 col-xs-4 text-center"><a data-toggle="tab" href="#menu2">Used tickets</a></li>
+                            </ul>
+                            <div className="tab-content">
+                                <AvailableTicket />
+                                <BuyTicket/>
+                                <UsedTicket/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        )
+    }
+}
 
 const mapDispatchToProps = (dispatch) => {
     return {
